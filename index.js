440a3780ee0a4c14bc38165542a3a209240e29a4
Brian Zhang <brianzhangw@gmail.com>
2018-07-24 02:34:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var play = []
+  
+  for(let i = 0; i < musicians.length; i++){
+    play.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  
+  return play;
+}
